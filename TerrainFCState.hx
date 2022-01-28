87c3397ff0e1b74a159b4ae3d7cca89e61883817
GithubBRichert <98623654+GithubBRichert@users.noreply.github.com>
2022-01-28 17:34:04 -0600

    polymod, UI, menus, editor menus, menu buttons.
    also includes HUD, health bar, and note code!

---
+++ TerrainFCState.hx
@@ -0,0 +1,20 @@
+get file TerrainFCsource.txt
+    //do not edit\\ get object polymod-source
+get script TerrainFCEngine.sourcescript
+get script TerrainFCEngine_FunkinHUD.Notes
+get script TerrainFCEngine_FunkinHUD.Counters
+get script TerrinFCEngine_FunkinHud.HealthBar
+get script TerrainFCEngine_FunkinUI.FreeplayMenu
+get script TerrainFCEngine_FunkinUI.StoryModeMenu
+get script TerrainFCEngine_FunkinUI.OptionsMenu
+get script TerrainFCEngine_FunkinData.ModSongsShowOnFreeplay
+get script TerrainFCEngine_FunkinData.ModWeeksShowOnStoryMode
+get script TerrainFCEngine_EditorMenus.StageEditor
+get script TerrainFCEngine_EditorMenus.ChartEditor
+get script TerrainFCEngine_EditorMenus.CharacterEditor
+get script TerrainFCEngine_FunkinUI.StoryModeButton
+get script TerrainFCEngine_FunkinUI.FreeplayButton
+get script TerrainFCEngine_FunkinUI.OptionsButton
+    
+    
+    
